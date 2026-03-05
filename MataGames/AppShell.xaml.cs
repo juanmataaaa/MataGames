802e251e97059551f00eb2c868446cb870d3dd85
Juan <Juanmata2003@gmail.com>
2026-03-05 18:09:11 +0100

    Agregar archivos de proyecto.

---
+++ MataGames/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MataGames
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MataGames/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MataGames")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MataGames.Pages")]
